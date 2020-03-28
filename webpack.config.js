d6d2b437db91dfcb3a9db8ae9188d5654f87ae78
Edward Lim <Edward.Lim@redcypress.com.tw>
2020-03-28 20:55:27 +0800

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = webpackConfigEnv => {
   const defaultConfig = singleSpaDefaults({
@@ -11,6 +12,10 @@ module.exports = webpackConfigEnv => {
 
   defaultConfig.output.filename = "app.js";
   defaultConfig.output.path = path.resolve(process.cwd(), "docs");
+
+  defaultConfig.plugins.push(
+    new CopyPlugin([{ from: "src/store.js", to: "store.js" }])
+  );
 
   const rxjsExternals = {
     externals: [/^rxjs\/?.*$/]
